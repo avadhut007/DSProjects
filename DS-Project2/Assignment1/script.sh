1f2ad676e19682f4d76a1b7b0c4e8ebfae52179d
avadhut007 <avadhut.talbar@gmail.com>
2022-11-08 23:57:52 -0600

    added max of clocks and printed

--- DS-Project2/Assignment1/script.sh
+++ DS-Project2/Assignment1/script.sh
@@ -1,3 +1,3 @@
-xterm -geometry 115x65 -fn 10x20 -hold -e  python process1.py & 
-xterm -geometry 115x65 -fn 10x20 -hold -e  python process2.py & 
-xterm -geometry 115x65 -fn 10x20 -hold -e  python process3.py & 
+xterm -geometry 115x65 -fn 10x20 -hold -e  python3 process1.py & 
+xterm -geometry 115x65 -fn 10x20 -hold -e  python3 process2.py & 
+xterm -geometry 115x65 -fn 10x20 -hold -e  python3 process3.py & 
